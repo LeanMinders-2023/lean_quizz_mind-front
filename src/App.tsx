8718d96a7a5d7101f07037b370bd8bdce1f910fd
Sstark97 <aitorscinfo@gmail.com>
2023-05-04 14:14:19 +0100

    Start with hexagonal architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Tabs from "./Tabs.tsx";
+import { Example } from './components/Example';
 
 const App = () => (
     <>
         <h1 className="text-center text-xl font-bold hover:text-red-400 lg:text-4xl">Vite + React</h1>
-        <Tabs />
+        <Example />
     </>
 );
 
