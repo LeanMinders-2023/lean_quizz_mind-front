c3bf2b8b1bd0750cd0888cbb07049a6427409dea
Sstark97 <aitorscinfo@gmail.com>
2023-05-04 14:27:06 +0100

    Add path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Example } from './components/Example';
+import { Example } from '@components/Example';
 
 const App = () => (
     <>
-        <h1 className="text-center text-xl font-bold hover:text-red-400 lg:text-4xl">Vite + React</h1>
+        <h1 className="text-center text-xl font-bold hover:text-red-400 lg:text-4xl">Lean Quizz Mind</h1>
         <Example />
     </>
 );
