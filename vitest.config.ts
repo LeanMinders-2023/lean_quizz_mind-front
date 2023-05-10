690de7702881974f06c40107daa8012e738ba23b
Javi <javiermespino@gmail.com>
2023-05-10 14:07:32 +0100

    Added path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,18 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
+
+const getAliases = () => {
+    return {
+        "@assets": resolve("src/assets"),
+        "@components": resolve("src/components"),
+        "@domain": resolve("src/domain"),
+        "@infrastructure": resolve("src/infrastructure"),
+        "@pages": resolve("src/pages"),
+    }
+}
 export default defineConfig({
     test: {
         includeSource: ['src/**/*.{ts,tsx}'],
@@ -9,4 +20,7 @@ export default defineConfig({
         environment: "happy-dom",
         setupFiles: ['./src/test/setup.ts'],
     },
+    resolve: {
+        alias: getAliases(),
+    }
 })
