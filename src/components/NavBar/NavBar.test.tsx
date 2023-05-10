b98fa85ed9a042666121b903d95d3aec487e7c4b
Sstark97 <aitorscinfo@gmail.com>
2023-05-10 12:59:24 +0100

    Test(🔴): renders correctly

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -1,9 +1,13 @@
 import { render } from "@testing-library/react";
+import {expect} from "vitest";
+
+const NavBar = () => null;
 
 describe("NavBar", () => {
     it("renders correctly", () => {
         const { container} = render(<NavBar />)
 
         expect(container).toBeInTheDocument()
+        expect(container).not.toBeEmptyDOMElement()
     })
 })
