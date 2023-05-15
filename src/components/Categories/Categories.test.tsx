3ef7b3846d3d52a2bd4c83f26a9c771075336dd0
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-05-15 14:29:05 +0200

    Test(🟢): Categories renders correctly

--- src/components/Categories/Categories.test.tsx
+++ src/components/Categories/Categories.test.tsx
@@ -1,8 +1,14 @@
 import {render} from "@testing-library/react";
+import {Categories} from "@components/Categories/Categories";
+import {Tab} from '@headlessui/react'
 
 describe("Categories", () => {
     it("renders correctly", () => {
-        const {container} = render(<Categories/>)
+        const {container} = render(
+            <Tab.Group>
+                <Categories categories={{}}/>
+            </Tab.Group>
+        )
 
         expect(container).toBeInTheDocument()
         expect(container).not.toBeEmptyDOMElement()
