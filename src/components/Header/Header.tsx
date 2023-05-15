c2dff737a270fa0871b3e7a251936d0c1b571a64
Roberto del Pino Hormiga <robertodelpino11@gmail.com>
2023-05-15 11:48:20 +0100

    🔵: Changed Home page adding Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import {NavBar} from "@components/NavBar/NavBar.tsx";
+import {ReactNode} from "react";
 
-export const Header = () => (
-    <header>
-        <NavBar/>
+interface ChildrenProps {
+    children: ReactNode
+}
+
+export const Header = ({children} : ChildrenProps) => (
+    <header className="bg-[#11111f] p-5">
+        <NavBar />
+        {children}
     </header>
 );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,7 @@
-import { NavBar } from "@components/NavBar/NavBar"
+import {Header} from "@components/Header/Header.tsx";
 
 export const Home = () => (
-    <header className="bg-[#11111f] p-5">
-        <NavBar />
+    <Header>
         <h1 className="mt-2 text-white text-center text-xl">Let's test your knowledge</h1>
-    </header>
+    </Header>
 );
