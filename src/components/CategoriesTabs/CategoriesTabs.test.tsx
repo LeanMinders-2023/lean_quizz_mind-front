be4b5b5d28f526cb393ef5b75abcf8cb87e81bc3
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-05-15 13:51:16 +0200

    Test(🟢): CategoriesTabs renders correctly

--- src/components/CategoriesTabs/CategoriesTabs.test.tsx
+++ src/components/CategoriesTabs/CategoriesTabs.test.tsx
@@ -1,4 +1,5 @@
 import {render} from "@testing-library/react";
+import CategoriesTabs from "@components/CategoriesTabs/CategoriesTabs";
 
 describe("CategoriesTabs", () => {
     it("renders correctly", () => {
