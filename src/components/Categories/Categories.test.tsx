ba2d7c2c7f92c9465a6fc095d73cea430ff45e90
Javi <javiermespino@gmail.com>
2023-05-16 14:09:07 +0100

    Test(🟢): Categories focus when clicked

--- src/components/Categories/Categories.test.tsx
+++ src/components/Categories/Categories.test.tsx
@@ -2,6 +2,8 @@ import {render, screen} from "@testing-library/react";
 import {Categories} from "@components/Categories/Categories";
 import {Tab} from '@headlessui/react'
 import {Category} from "@domain/category/category.model";
+import userEvent from "@testing-library/user-event"
+import {classNames} from "@components/CategoriesTabs/CategoriesTabs";
 
 describe("Categories", () => {
     it("renders correctly", () => {
@@ -26,5 +28,20 @@ describe("Categories", () => {
         const categoryInTab = screen.getByText(testingCategory.name)
 
         expect(categoryInTab).toBeInTheDocument()
+    })
+
+    it("focus when clicked", async () => {
+        const frontCategory: Category = {id: "1", name: "front"}
+        const backCategory: Category = {id: "2", name: "back"}
+        render(
+            <Tab.Group>
+                <Categories categories={[frontCategory, backCategory]} classNames={classNames}/>
+            </Tab.Group>
+        )
+
+        const backCategoryInTab = screen.getByText(backCategory.name)
+        await userEvent.click(backCategoryInTab)
+
+        expect(backCategoryInTab).toHaveClass("bg-white shadow")
     })
 })
