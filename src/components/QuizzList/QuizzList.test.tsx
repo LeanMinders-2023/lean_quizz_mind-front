338510d74cb10dee2851132d1592ab2a29863feb
Javi <javiermespino@gmail.com>
2023-05-17 12:41:51 +0100

    Test(🔴): QuizzList renders any items

--- src/components/QuizzList/QuizzList.test.tsx
+++ src/components/QuizzList/QuizzList.test.tsx
@@ -1,11 +1,27 @@
-import {render} from "@testing-library/react";
+import {render, screen, within} from "@testing-library/react";
 import {QuizzList} from "@components/QuizzList/QuizzList";
+import {QuizzDTO} from "@domain/quizz/quizz.model";
 
 describe("QuizzList", () => {
     it("renders correctly", () => {
-        const {container} = render(<QuizzList/>)
+        const {container} = render(<QuizzList />)
 
         expect(container).toBeInTheDocument()
         expect(container).not.toBeEmptyDOMElement()
+    })
+
+    it("renders any items", () => {
+        const quizzes: QuizzDTO[] = [{
+            name: "irrelevant",
+            image: "irrelevantImg",
+            difficulty: "irrelevant",
+            questions: []
+        }]
+        render(<QuizzList quizzes={quizzes}/>)
+
+        const allQuizzes = screen.getByRole("list")
+        const quizzItem = within(allQuizzes).getByRole("article")
+
+        expect(quizzItem).toBeInTheDocument()
     })
 })
