4b52b3c49a3f36f21cbcbb6d4422906131b9e012
Javi <javiermespino@gmail.com>
2023-05-17 12:32:55 +0100

    Test(🟢): QuizzList renders correctly

--- src/components/QuizzList/QuizzList.test.tsx
+++ src/components/QuizzList/QuizzList.test.tsx
@@ -1,5 +1,7 @@
 import {render} from "@testing-library/react";
 
+const QuizzList = () => <ul></ul>;
+
 describe("QuizzList", () => {
     it("renders correctly", () => {
         const {container} = render(<QuizzList/>)
