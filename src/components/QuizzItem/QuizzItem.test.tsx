ee7bd524ce70e1ce6a8b7b0a5a3c5081744527c8
Sstark97 <aitorscinfo@gmail.com>
2023-05-17 11:01:33 +0100

    Test(🔵): QuizzItem renders the cover

--- src/components/QuizzItem/QuizzItem.test.tsx
+++ src/components/QuizzItem/QuizzItem.test.tsx
@@ -1,17 +1,21 @@
 import {render, screen} from "@testing-library/react";
 import {QuizzItem} from "@components/QuizzItem/QuizzItem.tsx";
-import {expect} from "vitest";
+import {beforeEach, expect} from "vitest";
 import {QuizzDTO} from "@domain/quizz/quizz.model.ts";
 
 describe("QuizzItem", () => {
-    it("renders correctly", () => {
-        const quizz: QuizzDTO = {
+    let quizz: QuizzDTO
+
+    beforeEach(() => {
+        quizz = {
             name: "irrelevant",
             image: "irrelevantImg",
             difficulty: "irrelevant",
             questions: []
         }
+    })
 
+    it("renders correctly", () => {
         const {container} = render(<QuizzItem quizzDTO={quizz}/>)
 
         expect(container).toBeInTheDocument()
@@ -19,12 +23,6 @@ describe("QuizzItem", () => {
     })
 
     it("renders the cover", () => {
-        const quizz: QuizzDTO = {
-            name: "irrelevant",
-            image: "irrelevantImg",
-            difficulty: "irrelevant",
-            questions: []
-        }
         render(<QuizzItem quizzDTO={quizz}/>)
 
         const cover = screen.getByRole("img", { name: quizz.name })
