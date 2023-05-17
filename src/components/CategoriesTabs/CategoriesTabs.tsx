abef680a6c74b5e41eb930892f6df41f721f9535
Javi <javiermespino@gmail.com>
2023-05-17 13:08:54 +0100

    Test(🟢): CategoriesTabs does not contains quizzes

--- src/components/CategoriesTabs/CategoriesTabs.tsx
+++ src/components/CategoriesTabs/CategoriesTabs.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import {Tab} from '@headlessui/react'
 import {Categories} from "@components/Categories/Categories";
 import type {Category} from "@domain/category/category.model.ts";
+import {QuizzList} from "@components/QuizzList/QuizzList";
 
 export function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -17,46 +18,25 @@ export default function CategoriesTabs() {
         <div className="w-full max-w-md px-2 py-16 sm:px-0">
             <Tab.Group>
                 <Categories categories={categories} classNames={classNames} />
-                {/*<Tab.Panels className="mt-2">*/}
-                {/*    {Object.values(categories).map((posts, idx) => (*/}
-                {/*        <Tab.Panel*/}
-                {/*            key={idx}*/}
-                {/*            className={classNames(*/}
-                {/*                'rounded-xl bg-white p-3',*/}
-                {/*                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'*/}
-                {/*            )}*/}
-                {/*        >*/}
-                {/*            <ul>*/}
-                {/*                {posts.map((post) => (*/}
-                {/*                    <li*/}
-                {/*                        key={post.id}*/}
-                {/*                        className="relative rounded-md p-3 hover:bg-gray-100"*/}
-                {/*                    >*/}
-                {/*                        <h3 className="text-sm font-medium leading-5">*/}
-                {/*                            {post.title}*/}
-                {/*                        </h3>*/}
-
-                {/*                        <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">*/}
-                {/*                            <li>{post.date}</li>*/}
-                {/*                            <li>&middot;</li>*/}
-                {/*                            <li>{post.commentCount} comments</li>*/}
-                {/*                            <li>&middot;</li>*/}
-                {/*                            <li>{post.shareCount} shares</li>*/}
-                {/*                        </ul>*/}
-
-                {/*                        <a*/}
-                {/*                            href="#"*/}
-                {/*                            className={classNames(*/}
-                {/*                                'absolute inset-0 rounded-md',*/}
-                {/*                                'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'*/}
-                {/*                            )}*/}
-                {/*                        />*/}
-                {/*                    </li>*/}
-                {/*                ))}*/}
-                {/*            </ul>*/}
-                {/*        </Tab.Panel>*/}
-                {/*    ))}*/}
-                {/*</Tab.Panels>*/}
+                <Tab.Panels className="mt-2">
+                    {Object.values(categories).map((category, idx) => (
+                        <Tab.Panel
+                            key={idx}
+                            className={classNames(
+                                'rounded-xl bg-white p-3',
+                                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                            )}
+                        >
+                            {category.quizzes ? (
+                                <>
+                                    <QuizzList quizzes={category.quizzes} />
+                                </>
+                            ) : (
+                                <p>This category has not quizzes</p>
+                            )}
+                        </Tab.Panel>
+                    ))}
+                </Tab.Panels>
             </Tab.Group>
         </div>
     )
