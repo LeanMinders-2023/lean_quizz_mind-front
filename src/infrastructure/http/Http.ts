323310a5ddfdece99b6851901e527d591dbf9210
Sstark97 <aitorscinfo@gmail.com>
2023-05-18 14:13:27 +0100

    Test(🟢): Http should be defined if the url is not empty

--- src/infrastructure/http/Http.ts
+++ src/infrastructure/http/Http.ts
@@ -1,3 +1,9 @@
 export class Http {
+    private constructor(private url: string) {}
 
+    public static createInstance(url: string) {
+        if(url !== "") {
+            return new Http(url)
+        }
+    }
 }
