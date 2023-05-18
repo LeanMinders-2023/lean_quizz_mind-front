9707b98d6a57feb0591b656deb33370f2f269be7
Sstark97 <aitorscinfo@gmail.com>
2023-05-18 14:10:52 +0100

    Test(🔴): Http should be defined if the url is not empty

--- src/infrastructure/http/Http.test.ts
+++ src/infrastructure/http/Http.test.ts
@@ -1,8 +1,8 @@
 import {Http} from "@infrastructure/http/Http.ts";
 
 describe("Http", () => {
-    it("should be defined", () => {
-        const http = new Http();
+    it("should be defined if the url is not empty", () => {
+        const http = Http.createInstance("url");
 
         expect(http).toBeDefined();
     });
